b98cf7fa2794a9f49384984d990164672a7e4f4d
mike12345567 <me@michaeldrury.co.uk>
2022-08-12 16:32:33 +0100

    Updating datasource skeleton to include the schema.

--- datasource/src/index.ts
+++ datasource/src/index.ts
@@ -1,70 +1,7 @@
-import { IntegrationBase } from "@budibase/types"
-import fetch from "node-fetch"
+import datasource from "./datasource"
+import json from "../schema.json"
 
-interface Query {
-  method: string
-  body?: string
-  headers?: { [key: string]: string }
+export default {
+  integration: datasource,
+  schema: json,
 }
-
-class HTTPIntegration implements IntegrationBase {
-  private readonly url: string
-  private readonly cookie: string
-
-  constructor(config: { url: string; cookie: string }) {
-    this.url = config.url
-    this.cookie = config.cookie
-  }
-
-  async query(url: string, opts: Query) {
-    if (this.cookie) {
-      const cookie = { Cookie: this.cookie }
-      opts.headers = opts.headers ? { ...opts.headers, ...cookie } : cookie
-    }
-    const response = await fetch(url, opts)
-    if (response.status <= 300) {
-      return response.json()
-    } else {
-      const err = await response.text()
-      throw new Error(err)
-    }
-  }
-
-  async create(query: { json: object }) {
-    const opts = {
-      method: "POST",
-      body: JSON.stringify(query.json),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-    return this.query(this.url, opts)
-  }
-
-  async read(query: { queryString: string }) {
-    const opts = {
-      method: "GET",
-    }
-    return this.query(`${this.url}?${query.queryString}`, opts)
-  }
-
-  async update(query: { json: object }) {
-    const opts = {
-      method: "PUT",
-      body: JSON.stringify(query.json),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-    return this.query(this.url, opts)
-  }
-
-  async delete(query: { id: string }) {
-    const opts = {
-      method: "DELETE",
-    }
-    return this.query(`${this.url}/${query.id}`, opts)
-  }
-}
-
-export default HTTPIntegration
