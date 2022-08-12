98581c563f6bc3eaefc8bd35b3813e120352a51d
mike12345567 <me@michaeldrury.co.uk>
2022-08-12 13:00:50 +0100

    Adding Andrew's suggestions/removing warnings from datasource build.

--- datasource/rollup.config.js
+++ datasource/rollup.config.js
@@ -4,27 +4,19 @@ import json from "rollup-plugin-json"
 import copy from "rollup-plugin-copy2"
 import typescript from "@rollup/plugin-typescript"
 
-const ignoredWarnings = [
-  "unused-export-let",
-  "css-unused-selector",
-  "module-script-reactive-declaration",
-  "a11y-no-onchange",
-]
-
 export default {
   input: "src/index.js",
   output: {
     sourcemap: false,
     format: "cjs",
     file: "dist/plugin.min.js",
-    name: "plugin",
   },
   plugins: [
-    typescript(),
     resolve({
       preferBuiltins: true,
       browser: false,
     }),
+    typescript(),
     json(),
     terser(),
     copy({
