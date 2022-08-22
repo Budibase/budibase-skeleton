1bf700104d067d9050a54433315c143aeb1d8223
Andrew Kingston <andrew@kingston.dev>
2022-08-22 18:28:55 +0100

    Revert to using version from package.json when initialising components

--- component/src/index.js
+++ component/src/index.js
@@ -1,8 +1,9 @@
 import Component from "./Component.svelte"
 import schema from "../schema.json"
+import pkg from "../package.json"
 
 if (window) {
-  const plugin = { Component, schema }
+  const plugin = { Component, schema, version: pkg.version }
   if (!window["##BUDIBASE_CUSTOM_COMPONENTS##"]) {
     window["##BUDIBASE_CUSTOM_COMPONENTS##"] = []
   }
