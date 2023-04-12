191b23e9de0c41fdfd0d295dfd3ae31a41a69cb8
mike12345567 <me@michaeldrury.co.uk>
2023-04-12 15:56:03 +0100

    Updating automation test script.

--- automation/test/index.spec.ts
+++ automation/test/index.spec.ts
@@ -1,5 +1,25 @@
-import { describe, it, beforeAll, expect } from "@jest/globals"
+import automation from "../src"
+import { describe, it, expect } from "@jest/globals"
+import { EventEmitter } from "events"
 
 describe("test the automation action", () => {
-
+  it("should be able to run automation step", async () => {
+    const response = await automation.action({
+      inputs: {
+        text: "hello",
+      },
+      emitter: new EventEmitter(),
+      context: {},
+      appId: "test",
+    })
+    expect(response.success).toBe(true)
+  })
 })
+
+describe("test the automation schema", () => {
+  it("should provide the schema", () => {
+    expect(automation.schema.type).toBe("automation")
+    expect(automation.schema.schema.stepId).toBeDefined()
+  })
+})
+
